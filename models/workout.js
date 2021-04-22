2bc88782c437ad4f1ce2d0bfb8263338c81f47ce
Tolga Secme <tolgasecme@icloud.com>
2021-04-22 16:30:17 -0600

    server.js/workout model created

---
+++ models/workout.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                required: true,
+                trim: true
+            },
+            name: {
+                type: String,
+                required: true,
+                unique: true
+            },
+            distance: {
+                type: Number
+            },
+            duration: {
+                type: Number
+            },
+            weight: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+
+        }
+    ]
+})
+
+const Workout = mongoose.model("Workout", workoutSchema);
+module.exports = Workout;
