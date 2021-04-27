a4b9d70910b2c6f2cb5af6d60ba73b20e99dc39a
Tolga Secme <tolgasecme@icloud.com>
2021-04-26 23:09:01 -0600

    routes

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,16 +1,17 @@
 const router = require("express").Router();
 const path = require("path");
 
+router.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
 router.get("/exercise", (req,res) => {
-    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+    res.sendFile(path.join(__dirname, "exercise.html"));
 });
 
 router.get("/stats", (req,res) => {
-    res.sendFile(path.join(__dirname, "../public/stats.html"));
+    res.sendFile(path.join(__dirname, "stats.html"));
 });
 
-router.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-});
 
 module.exports = router;
